4c7eff27ae1895fb4463670dd9db55e06c41b2b6
e1518118 <e1518118@u.nus.edu>
2025-11-05 12:51:35 +0800

    UDP simulation with delays and packet loss

---
+++ udp_server.py
@@ -0,0 +1,22 @@
+# server
+import socket
+import asyncio
+
+# Define server address and port
+HOST = "127.0.0.1"    #"0.0.0.0"  # localhost
+PORT = 9999
+async def run_server():
+    # Create UDP socket
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_socket:
+        server_socket.bind((HOST, PORT))
+        print(f"UDP server listening on {HOST}:{PORT}...")
+
+        while True:
+            data, addr = server_socket.recvfrom(1024)  # buffer size is 1024 bytes
+            print(f"Received message from {addr}: {data.decode()}")
+            
+            # Echo message back to client
+            server_socket.sendto(b"Message received: " + data, addr)
+
+if __name__ == "__main__":
+    asyncio.run(run_server())
